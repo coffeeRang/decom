0890f050d6037ac12d22744f04414ac0d04babf8
devJang <devjang2016@gmail.com>
2018-08-16 23:56:39 +0900

    init branch

--- src/components/Test1.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Test1 = () => <div>Test1</div>;
-
-export { Test1 };
--- src/components/Test2.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Test2 = () => <div>Test2</div>;
-
-export { Test2 };
